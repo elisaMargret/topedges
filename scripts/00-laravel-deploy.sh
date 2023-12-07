12586a8979a593c9c4a8fd1ae614727fa8d3635b
elisaMargret <e04091069@gmail.com>
2023-12-07 09:47:09 +0100

    file updateed

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -14,8 +14,8 @@ php artisan config:cache
 echo "Caching routes..."
 php artisan route:cache
 
-echo "Running migrations..."
-php artisan migrate --force
+# echo "Running migrations..."
+# php artisan migrate --force
 
 # echo "Running db:seed"
 # php artisan db:seed
