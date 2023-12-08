6796c3b3dc76db5b30cb86e5bc624d8cb713d01b
elisaMargret <e04091069@gmail.com>
2023-12-08 09:19:43 +0100

    file updated

--- routes/web.php
+++ routes/web.php
@@ -84,32 +84,32 @@ Route::prefix('wallet')->group(function () {
 // Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 // Admin
-// Route::group(['prefix' => 'admin'],function(){
+Route::group(['prefix' => 'admin'],function(){
 
-//     Route::get('/login', [LoginController::class, 'index'])->name('admin.login');
-//     Route::get('/reset-password', [LoginController::class, 'resetPassword'])->name('admin.reset-password');
-//     Route::post('login', [LoginController::class,'login' ])->name('admin.login.post');
+    Route::get('/', [LoginController::class, 'index'])->name('admin.login');
+    Route::get('reset-password', [LoginController::class, 'resetPassword'])->name('admin.reset-password');
+    Route::post('login', [LoginController::class,'login' ])->name('admin.login.post');
     
-//     Route::post('logout', [LoginController::class, 'logout'])->name('admin.logout');
-    
-    
-//     Route::resource('customers', UserController::class)->names([
-//         'index' => 'admin.customers',
-//         'show' => 'admin.customer.show',
-//         'edit' => 'admin.customer.edit',
-//         'update' => 'admin.customer.update',
-//         'destroy' => 'admin.customer.delete',
-//     ]);
-    
-//     Route::put('customer/wallet', [UserController::class, 'updateWallet'])->name('admin.customer.wallet.update');
-//     Route::put('customer/{customer}/wallet/{wallet}', [WalletController::class, 'approve'])->name('admin.customer.transaction.approve');
-//     Route::delete('customer/{customer}/wallet/{wallet}', [WalletController::class, 'destroy'])->name('admin.customer.transaction.delete');
-    
-//     Route::get('dashboard', [DashboardController::class, 'index'])->name('admin.dashboard');
+    Route::post('logout', [LoginController::class, 'logout'])->name('admin.logout');
     
     
-//     Route::resource('wallets', WalletController::class)->names([
-//         'index' => 'admin.wallets'
-//     ]);
+    Route::resource('customers', UserController::class)->names([
+        'index' => 'admin.customers',
+        'show' => 'admin.customer.show',
+        'edit' => 'admin.customer.edit',
+        'update' => 'admin.customer.update',
+        'destroy' => 'admin.customer.delete',
+    ]);
     
-// });
+    Route::put('customer/wallet', [UserController::class, 'updateWallet'])->name('admin.customer.wallet.update');
+    Route::put('customer/{customer}/wallet/{wallet}', [WalletController::class, 'approve'])->name('admin.customer.transaction.approve');
+    Route::delete('customer/{customer}/wallet/{wallet}', [WalletController::class, 'destroy'])->name('admin.customer.transaction.delete');
+    
+    Route::get('dashboard', [DashboardController::class, 'index'])->name('admin.dashboard');
+    
+    
+    Route::resource('wallets', WalletController::class)->names([
+        'index' => 'admin.wallets'
+    ]);
+    
+});
